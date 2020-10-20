f8de82765a6d12d53d2261dd9ff84017680a2b6a
Neha-Aggarwal12 <f2015554p@alumni.bits-pilani.ac.in>
2020-10-20 14:19:21 +0530

    commit files

---
+++ java-programs/Demo.java
@@ -0,0 +1,14 @@
+ // first Java program here 
+// its use for comments 
+
+public class Demo {
+
+// main method here as entry point 
+
+public static void main(String[] args){
+
+System.out.println("Ref in System");
+
+}
+
+}
